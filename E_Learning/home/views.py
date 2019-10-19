84f7c1d778e4099d1950777e90738fd7c6f539cf
FalakChhikara <45658281+FalakChhikara@users.noreply.github.com>
2019-10-20 04:36:42 +0530

    Update views.py

--- E_Learning/home/views.py
+++ E_Learning/home/views.py
@@ -22,36 +22,91 @@ database = firebase.database()
 def dashboard(request):
     return render(request, "accounts/dashboard.html")
 
+#
+# def checkin(request):
+#     if request.method == 'GET':
+#         return render(request, "accounts/checkin.html")
+#     else:
+#         course = request.POST.get('course')
+#         question = request.POST.get('question')
+#         data = {
+#             "course": course,
+#             "question": question,
+#         }
+#         try:
+#             idtoken = request.session["uid"]
+#             a = authe.get_account_info(idtoken)
+#             a = a['users']
+#             a = a[0]
+#             a = a['localId']
+#             database.child('users').child(a).child('Q&A').child('question').set(data,idtoken)
+#             ##########
+#             name = database.child("users").child(a).child("details").child("branch").get(idtoken).val()
+#
+#             ###########
+#             return render(request, "accounts/homepage.html",{'name':name})
+#         except:
+#             return render(request, "accounts/login.html", {'error': 'oops login first'})
 
-def checkin(request):
+
+def add_notes(request):
     if request.method == 'GET':
-        return render(request, "accounts/checkin.html")
+        return render(request, "home/add_notes.html")
+
     else:
-        course = request.POST.get('course')
-        question = request.POST.get('question')
-        data = {
-            "course": course,
-            "question": question,
-        }
+        notes_name = request.POST.get('notes_name')
+        tags = request.POST.get('tags')
+        url = request.POST.get('url')
+        print(url)
+
+
         try:
-            idtoken = request.session["uid"]
+            idtoken = request.session['uid']
             a = authe.get_account_info(idtoken)
             a = a['users']
             a = a[0]
             a = a['localId']
-            database.child('users').child(a).child('Q&A').child('question').set(data,idtoken)
-            ##########
-            name = database.child("users").child(a).child("details").child("branch").get(idtoken).val()
-
-            ###########
-            return render(request, "accounts/homepage.html",{'name':name})
+            # a contains local id
         except:
-            return render(request, "accounts/login.html", {'error': 'oops login first'})
+            return render(request, "accounts/login.html")
 
+        # storage = firebase.storage()
+        # data1 = {
+        #     "url": url,
+        # }
+        # storage.child("images/bank-details.jpg").put("bank-details.jpg")
 
-def add_notes(request, user):
-    if request.method == 'GET':
-        return render(request, "home/add_notes.html",{'user':user})
+        username = database.child("users").child(a).child("details").child("username").get(idtoken).val()
+        #print(username)
+        data = {
+            "tags": tags,
+            "url": url,
+            "username": username,
+        }
 
-    else: pass
+        database.child('Notes').child(notes_name).set(data, idtoken)
+        return render(request, "home/homepage.html")
+
+def view_notes(request):
+    notes = database.child('Notes').shallow().get().val()
+    print(notes)
+    list_notes = [*notes]
+    print(list_notes)
+    tags = []
+    urls = []
+    usernames = []
+    for i in list_notes:
+        tag = database.child('Notes').child(i).child('tags').get().val()
+        tags.append(tag)
+        url = database.child('Notes').child(i).child('url').get().val()
+        urls.append(url)
+        username = database.child('Notes').child(i).child('username').get().val()
+        usernames.append(username)
+
+    print(tags)
+    print(urls)
+    print(usernames)
+    combine_list = zip(list_notes,tags,usernames,urls)
+    return render(request, "home/display_notes.html", {'combine_list':combine_list})
+
 
