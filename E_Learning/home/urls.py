89dcc7369719ffdfbe89f7daa868fef00b87b227
FalakChhikara <45658281+FalakChhikara@users.noreply.github.com>
2019-10-20 17:44:50 +0530

    Update urls.py

--- E_Learning/home/urls.py
+++ E_Learning/home/urls.py
@@ -1,21 +1,20 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
 app_name = "home"
 
 urlpatterns = [
     path(r'add_notes/', views.add_notes, name="add_notes"),
-
     path(r'view_notes/', views.view_notes, name="view_notes"),
+    path(r'add_club/', views.add_club, name="add_club"),
+    path('display_clubs/', views.display_clubs, name='display_clubs'),
+    path('addbook/',views.addbook,name='addbook'),
+    path('displaybook/',views.displaybook,name='displaybook'),
+    path('(?P<username>[\w.@+-]+)/(?P<book_title>[\w.@+-]+)/requestbook/',views.requestbook, name='requestbook'),
+    path('viewrequests/',views.viewrequests, name='viewrequests'),
 
-    # path(r'/login', views.login, name="login"),
-
-    # path(r'/signup', views.signup, name="signup"),
-    # path(r'/logout', views.logout, name="logout"),
-    path('addBook/',views.addbook,name='addbook'),
-   #path('searchbooks/',views.searchbooks,name='search')
-   #path('listbooks/',views.listbooks,name='postsearch')
-    #path('deletebooks/')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
